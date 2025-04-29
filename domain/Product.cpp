e8187759328385347524584cc2d2b5e20f7f4562
klooseduard95 <eduard.kloos@stud.ubbcluj.ro>
2025-04-29 18:40:52 +0300

    P1 done

--- domain/Product.cpp
+++ domain/Product.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Product.h"
+
+Product::Product(const string& productId,
+                 const string& name,
+                 double price,
+                 int stock)
+    : productId(productId), name(name), price(price), stock(stock) {}
+
+string Product::getId() const {
+    return productId;
+}
+
+string Product::getName() const {
+    return name;
+}
+
+double Product::getPrice() const {
+    return price;
+}
+
+int Product::getStock() const {
+    return stock;
+}
+
+void Product::setPrice(double newPrice) {
+    price = newPrice;
+}
+
+void Product::setStock(int newStock) {
+    stock = newStock;
+}
+
+
+
+
+
--- domain/Product.h
+++ domain/Product.h
@@ -1,8 +1,30 @@
 //
 // Created by Eduard-Andreas Kloos on 17.04.2025.
 //
-
+#pragma once
+#include <string>
+using namespace std;
 #ifndef PRODUCT_H
 #define PRODUCT_H
+
+class Product {
+private:
+    string productId, name;
+    double price;
+    int stock;
+public:
+    Product(const string& productId,
+        const string& name,
+        double price,
+        int stock);
+
+    [[nodiscard]] string getId() const;
+    [[nodiscard]] string getName() const;
+    [[nodiscard]] double getPrice() const;
+    [[nodiscard]] int getStock() const;
+
+    void setPrice(double);
+    void setStock(int);
+};
 
 #endif //PRODUCT_H
