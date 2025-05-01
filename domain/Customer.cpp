e9d0554ba50d92439364f24cee00389d33a92048
Alisa Faloba <alisa.faloba@stud.ubbcluj.ro>
2025-05-01 23:32:46 +0300

    m1+k1

--- domain/Customer.cpp
+++ domain/Customer.cpp
@@ -2,4 +2,21 @@
 // Created by Eduard-Andreas Kloos on 17.04.2025.
 //
 
-#include "Client.h"
+#include "Customer.h"
+
+Customer::Customer(const std::string& email, const std::string& password,
+                   const std::string& firstName, const std::string& lastName,
+                   const std::string& address, const std::string& note,
+                   bool gdprDeleted)
+    : User(email, password, "customer"),
+      firstName(firstName), lastName(lastName),
+      address(address), note(note), gdprDeleted(gdprDeleted) {}
+
+std::string Customer::getFirstName() const { return firstName; }
+std::string Customer::getLastName() const { return lastName; }
+std::string Customer::getAddress() const { return address; }
+std::string Customer::getNote() const { return note; }
+bool Customer::isGdprDeleted() const { return gdprDeleted; }
+
+void Customer::setGdprDeleted(bool value) { gdprDeleted = value; }
+
