210c77fe3305247abb452ff2c467d19f73188660
Jarda Mihai <mihai.jarda@stud.ubbcluj.ro>
2025-05-08 13:56:05 +0300

    Inceputul de la T_A

---
+++ test/Test_A.cpp
@@ -0,0 +1,7 @@
+#include "Test_A.h"
+#include "../repository/InMemoryRepository.cpp"
+#include "../repository/UserRepository.cpp"
+#include "../services/UserService.cpp"
+#include "../ui/UserManagementUI.cpp"
+#include "../controller/UserManagementController.cpp"
+
---
+++ test/Test_A.h
@@ -0,0 +1,11 @@
+//
+// Created by Mitza on 5/8/2025.
+//
+
+#ifndef TEST_A_H
+#define TEST_A_H
+class Test_A{
+  public:
+    void test_All();
+};
+#endif //TEST_A_H
