04f5cf7aaf0dc8005d6864f8ce69c1980c2b050c
Alisa Faloba <alisa.faloba@stud.ubbcluj.ro>
2025-05-15 12:43:02 +0300

    k2 done

--- controller/ClientManagementController.h
+++ controller/ClientManagementController.h
@@ -1,8 +1,41 @@
 //
-// Created by Eduard-Andreas Kloos on 17.04.2025.
+// Created by Eduard-Andreas Kloos on [Datum].
 //
 
-#ifndef CLIENTMANAGEMENTCONTROLLER_H
-#define CLIENTMANAGEMENTCONTROLLER_H
+#pragma once
 
-#endif //CLIENTMANAGEMENTCONTROLLER_H
+#include "../domain/Customer.h"
+#include "../repository/interfaces/IRepository.h"
+#include <vector>
+#include <string>
+
+#ifndef CUSTOMERCONTROLLER_H
+#define CUSTOMERCONTROLLER_H
+
+class CustomerController {
+private:
+    IRepository<Customer>* customerRepository;
+
+public:
+    CustomerController(IRepository<Customer>* repo);
+
+
+    bool createCustomer(const std::string& email, const std::string& password,
+                        const std::string& firstName, const std::string& lastName,
+                        const std::string& address, const std::string& note = "",
+                        bool gdprDeleted = false);
+
+
+    bool updateCustomer(const Customer& updatedCustomer);
+
+
+    bool deleteCustomer(const std::string& email);
+
+
+    std::vector<Customer> listAllCustomersSorted() const;
+
+
+    Customer findCustomerByEmail(const std::string& email) const;
+};
+
+#endif // CUSTOMERCONTROLLER_H
