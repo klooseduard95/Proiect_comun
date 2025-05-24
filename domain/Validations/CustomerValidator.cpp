b66c78c28367bea312291dd6690a699005ac95d9
Jarda Mihai <mihai.jarda@stud.ubbcluj.ro>
2025-05-24 15:24:50 +0300

    Terminat validari la customer

--- domain/Validations/CustomerValidator.cpp
+++ domain/Validations/CustomerValidator.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "CustomerValidator.h"
+#include "stdexcept"
+#include <string>
+#include <regex>
+#include "../repository/InMemoryRepository.h"
+
+using namespace std;
+
+void CustomerValidator::ValidateEmail(const Customer &customer) {
+    InMemoryRepository<Customer> repository;
+    if (std::regex pattern(R"(\w+@\w+\.\w+)"); !regex_match(customer.getEmail(), pattern)) {
+        throw invalid_argument ("Invalid email format.");
+    }
+    auto customer2 = repository.getById(customer.getId());
+    if (customer2.getEmail() == customer.getEmail()) {
+        throw invalid_argument ("Email already exists.");
+    }
+};
+
+void CustomerValidator::ValidateGdpr(const Customer &customer) {
+    if (customer.isGdprDeleted()) {
+        if (customer.getFirstName().empty() || customer.getLastName().empty() || customer.getAddress().empty()) {
+            throw invalid_argument ("All fields need to be completed.");
+        }
+    }
+    else {
+        if (customer.getFirstName().empty() || customer.getLastName().empty()) {
+            throw invalid_argument ("First and lastname need to be completed.");
+        }
+    }
+}
--- domain/Validations/CustomerValidator.h
+++ domain/Validations/CustomerValidator.h
@@ -1,8 +1,14 @@
 //
 // Created by Eduard-Andreas Kloos on 29.04.2025.
 //
+#pragma once
+#include "Customer.h"
 
 #ifndef CUSTOMERVALIDATOR_H
 #define CUSTOMERVALIDATOR_H
-
+class CustomerValidator {
+public:
+    static void ValidateEmail(const Customer& customer);
+    static void ValidateGdpr(const Customer& customer);
+};
 #endif //CUSTOMERVALIDATOR_H
